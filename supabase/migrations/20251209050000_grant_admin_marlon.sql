a23d9e1cef599a9e3e013bcb06137baf46b73f7f
Marlon Carvalho <marloncrs79@gmail.com>
2025-12-09 01:35:37 -0300

    chore: grant admin privileges to marlon

---
+++ supabase/migrations/20251209050000_grant_admin_marlon.sql
@@ -0,0 +1,8 @@
+-- Grant admin privileges to specific user
+UPDATE auth.users
+SET raw_user_meta_data = jsonb_set(
+  COALESCE(raw_user_meta_data, '{}'::jsonb),
+  '{is_admin}',
+  'true'
+)
+WHERE email = 'marloncrs79@gmail.com';
